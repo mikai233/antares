76681ca86c640ddc32dba3dac1b436196d93f1dd
mikai <dreamfever2017@yahoo.com>
2023-06-15 00:08:54 +0800

    AssetPackage

--- shared/src/main/kotlin/com/mikai233/shared/assets/AssetPackage.kt
+++ shared/src/main/kotlin/com/mikai233/shared/assets/AssetPackage.kt
@@ -1,12 +1,30 @@
 package com.mikai233.shared.assets
 
-class AssetPackage(private val assets: MutableList<Asset>, val merged: Boolean) {
+class AssetPackage(val assets: List<Asset>, val merged: Boolean) {
 
     companion object {
-        fun builder():
+        fun builder(): AssetPackageBuilder = AssetPackageBuilder()
     }
 
     fun isHybrid(): Boolean {
         return assets.groupBy { it.type }.size == 1
+    }
+
+    fun isEmpty(): Boolean = assets.isEmpty()
+
+    fun isNotEmpty(): Boolean = !isEmpty()
+
+    fun isLogicEmpty(): Boolean = assets.all { it.num == 0L }
+
+    fun isLogicNotEmpty(): Boolean = assets.any { it.num != 0L }
+
+    fun isAllPositive(): Boolean = assets.all { it.num > 0L }
+
+    fun toBuilder(): AssetPackageBuilder {
+        return AssetPackageBuilder().plus(this)
+    }
+
+    operator fun plus(other: AssetPackage): AssetPackage {
+        return toBuilder().plus(other).build()
     }
 }
--- shared/src/main/kotlin/com/mikai233/shared/assets/AssetPackageBuilder.kt
+++ shared/src/main/kotlin/com/mikai233/shared/assets/AssetPackageBuilder.kt
@@ -1,17 +1,23 @@
 package com.mikai233.shared.assets
 
-class AssetPackageBuilder private constructor(
-    private val assets: MutableList<Asset> = mutableListOf(),
-    private var mergeAsset: Boolean = true
-) {
+class AssetPackageBuilder {
+    private var assets: MutableList<Asset> = mutableListOf()
+    private var mergeOnBuild: Boolean = true
 
-    fun autoMerge(merge: Boolean): AssetPackageBuilder {
-        mergeAsset = merge
+    fun mergeOnBuild(merge: Boolean): AssetPackageBuilder {
+        mergeOnBuild = merge
         return this
     }
 
-    fun mergePrevious(): AssetPackageBuilder {
-        TODO()
+    fun merge(): AssetPackageBuilder {
+        assets = assets.groupBy { it.type }.mapNotNull { (_, typeAssets) ->
+            if (typeAssets.isNotEmpty()) {
+                typeAssets.reduce { acc, asset -> acc + asset }
+            } else {
+                null
+            }
+        }.toMutableList()
+        return this
     }
 
     operator fun plus(asset: Asset): AssetPackageBuilder {
@@ -19,5 +25,62 @@ class AssetPackageBuilder private constructor(
         return this
     }
 
-    fun build(): AssetPackage = AssetPackage(assets, mergeAsset)
+    operator fun plus(otherAssets: List<Asset>): AssetPackageBuilder {
+        assets.addAll(otherAssets)
+        return this
+    }
+
+    operator fun plus(otherPackage: AssetPackage): AssetPackageBuilder {
+        assets.addAll(otherPackage.assets)
+        return this
+    }
+
+    operator fun minus(asset: Asset): AssetPackageBuilder {
+        assets.add(asset.new(-asset.num))
+        return this
+    }
+
+    operator fun minus(otherAssets: List<Asset>): AssetPackageBuilder {
+        assets.addAll(otherAssets.map { it.new(-it.num) })
+        return this
+    }
+
+    operator fun times(scale: Int): AssetPackageBuilder {
+        assets = assets.map { it * scale }.toMutableList()
+        return this
+    }
+
+    operator fun times(scale: Long): AssetPackageBuilder {
+        assets = assets.map { it * scale }.toMutableList()
+        return this
+    }
+
+    fun times(scale: Double, strategy: RoundingStrategy, mergeBeforeTimes: Boolean) {
+        if (mergeBeforeTimes) {
+            merge()
+        }
+        assets = assets.map { it.times(scale, strategy) }.toMutableList()
+    }
+
+    fun cleanupZero(): AssetPackageBuilder {
+        assets = assets.filter { it.num != 0L }.toMutableList()
+        return this
+    }
+
+    fun cleanupNonPositive(): AssetPackageBuilder {
+        assets = assets.filter { it.num > 0 }.toMutableList()
+        return this
+    }
+
+    fun cleanupNonNegative(): AssetPackageBuilder {
+        assets = assets.filter { it.num >= 0 }.toMutableList()
+        return this
+    }
+
+    fun build(): AssetPackage {
+        if (mergeOnBuild) {
+            merge()
+        }
+        return AssetPackage(assets, mergeOnBuild)
+    }
 }
