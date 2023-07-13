4e61b9cde6fa6fcfe0d833a76a37209f9d92181b
mikai <dreamfever2017@yahoo.com>
2023-07-13 23:09:39 +0800

    opt data manager

--- common/src/main/kotlin/com/mikai233/common/annotation/GenerateLoader.kt
+++
@@ -1,7 +0,0 @@
-package com.mikai233.common.annotation
-
-import akka.actor.typed.javadsl.AbstractBehavior
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.CLASS)
-annotation class GenerateLoader(val actor: KClass<out AbstractBehavior<*>>)
--- common/src/main/kotlin/com/mikai233/common/annotation/LoadMe.kt
+++
@@ -1,4 +0,0 @@
-package com.mikai233.common.annotation
-
-@Target(AnnotationTarget.PROPERTY)
-annotation class LoadMe
