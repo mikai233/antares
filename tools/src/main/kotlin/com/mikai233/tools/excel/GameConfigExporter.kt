c099186893742a4dd31c976d4b8c87b3bd01f6eb
mikai <dreamfever2017@yahoo.com>
2025-10-22 14:08:01 +0800

    feat: auto-read version from resources on config export

--- tools/src/main/kotlin/com/mikai233/tools/excel/GameConfigExporter.kt
+++ tools/src/main/kotlin/com/mikai233/tools/excel/GameConfigExporter.kt
@@ -2,6 +2,7 @@ package com.mikai233.tools.excel
 
 import com.beust.jcommander.JCommander
 import com.beust.jcommander.Parameter
+import com.google.common.io.Resources
 import com.mikai233.common.conf.GlobalEnv
 import com.mikai233.common.excel.GameConfigManagerSerde
 import com.mikai233.common.excel.createGameConfigManager
@@ -16,12 +17,14 @@ private class ExporterCli {
     @Parameter(names = ["-e", "--excel"], description = "excel path", required = true)
     lateinit var excelPath: String
 
-    @Parameter(names = ["-v", "--version"], description = "game version", required = true)
-    lateinit var version: String
+    @Parameter(names = ["-v", "--version"], description = "game version")
+    var version: String = Resources.getResource("version").readText()
 
     @Parameter(names = ["-f", "--file"], description = "generate file or upload to zookeeper")
     var file: Boolean = false
 }
+
+private const val BIN_NAME = "game_configs_bin.tar.gz"
 
 suspend fun main(args: Array<String>) {
     val exporterCli = ExporterCli()
@@ -30,6 +33,7 @@ suspend fun main(args: Array<String>) {
         .addObject(exporterCli)
         .build()
         .parse(*args)
+    println("Generate config from excel path: ${exporterCli.excelPath}, version: ${exporterCli.version}")
     val client = asyncZookeeperClient(GlobalEnv.zkConnect)
     val manager = createGameConfigManager(
         exporterCli.excelPath,
@@ -39,14 +43,16 @@ suspend fun main(args: Array<String>) {
         withContext(Dispatchers.IO) {
             GameConfigManagerSerde.serialize(
                 manager,
-                FileOutputStream("game_configs_bin.tar.gz"),
+                FileOutputStream(BIN_NAME),
             )
         }
+        println("Generated config file: $BIN_NAME")
     } else {
         val bytes = GameConfigManagerSerde.serialize(manager)
         client.create()
             .withOptions(setOf(CreateOption.setDataIfExists, CreateOption.createParentsIfNeeded))
             .forPath(com.mikai233.common.config.GAME_CONFIG, bytes)
             .await()
+        println("Uploaded config file to Zookeeper path: ${com.mikai233.common.config.GAME_CONFIG}")
     }
 }
